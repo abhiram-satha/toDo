3bdd4ce79aaa6ed7f5abb838bc4eba98752b04b1
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-08 23:09:01 +0000

    Installing dependencies and required files

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,26 @@
-const express = require("express")
-
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 const app = express();
+
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+
+const path = require("path");
 const PORT = 8080;
 
-app.listen(PORT, ()=> {
-  console.log(`Listening on port ${PORT}`)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
---
+++ backend/db/seeds/seeds.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY NOT NULL,
+--     email VARCHAR(30) NOT NULL UNIQUE,
+--     password VARCHAR(90) NOT NULL,
+--     username VARCHAR(30) NOT NULL UNIQUE
+-- );
+
