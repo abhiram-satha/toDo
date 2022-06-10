e0c5e3280c0f38b92bf15638fe02cbae9cc6a65a
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-10 03:56:08 +0000

    Rendering card from mui

--- todo/src/components/NewToDo.jsx
+++ todo/src/components/NewToDo.jsx
@@ -1,9 +1,27 @@
-export default function NewToDo({newItems}) {
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-  console.log(newItems)
-  return(
-    <div>
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function NewToDo({newItems}) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
       This is New
-    </div>
-  )
+      </CardContent>
+      
+    </Card>
+  );
 }
