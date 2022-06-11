6ea92a49853372a2d38fefb0c8e6ef7a8b5aa084
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-11 02:53:12 +0000

    Renders the to do list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,42 +1,53 @@
-import './App.css';
-import AddToDo from './components/AddToDo';
-import ToDoList from './components/ToDoList';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import "./App.css";
+import AddToDo from "./components/AddToDo";
+import ToDoList from "./components/ToDoList";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
 
-  
-  const [toDos, setToDos] = useState([])
-
-  useEffect(()=> {
+  useEffect(() => {
     async function getPosts() {
-      const results = await axios('http://localhost:8080/api/posts')
-      setToDos(results.data.posts)
+      const results = await axios("http://localhost:8080/api/posts");
+      setToDos(results.data.posts);
     }
 
-    getPosts()
-  }, [])
+    getPosts();
+  }, []);
 
   const createNewToDo = (event) => {
     event.preventDefault();
 
-    const userInput = event.target[0].value
-    axios.post('http://localhost:8080/api/posts', {
-      user_id: 1,
-      message: userInput,
-      status_id: 1
-    })
-  }
+    const userInput = event.target[0].value;
+    axios
+      .post("http://localhost:8080/api/posts", {
+        user_id: 1,
+        message: userInput,
+        status_id: 1,
+      })
+      .then(()=> {
+        return axios('http://localhost:8080/api/posts')
+      })
+      .then(result => setToDos(result.data.posts))
+      
+      
+      // .then(()=> getPosts())
+      //   function getPosts() {
+      //   const results = axios('http://localhost:8080/api/posts')
+      //   .then(()=> setToDos(results.data.posts))
+        
+      // }
+
+      
+      event.target[0].value = "";
+  };
 
   return (
     <div className="App">
+      <AddToDo createNewToDo={createNewToDo} />
 
-
-      <AddToDo createNewToDo={createNewToDo}/>
-
-      {toDos.length >= 1 && <ToDoList listOfToDos={toDos}/>}
-      
+      {toDos.length >= 1 && <ToDoList listOfToDos={toDos} />}
     </div>
   );
 }
