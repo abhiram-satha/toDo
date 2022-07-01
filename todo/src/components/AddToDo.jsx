042993d7d4f02c44b721e105e458735f7946c408
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-01 00:40:29 +0000

    Adding css to addToDo form

--- todo/src/components/AddToDo.jsx
+++ todo/src/components/AddToDo.jsx
@@ -1,9 +1,33 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import "./AddToDo.css"
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
 export default function AddToDo ({createNewToDo}) {
   return(
     <>
-    <form onSubmit={(event)=>{createNewToDo(event)}}>
+    <Card sx={{ minWidth: 350 }} className="cardContent addNewToDo">
+      <CardContent >
+      <form onSubmit={(event)=>{createNewToDo(event)}}>
       <input type="text" placeholder="Add New To Do"></input>
     </form>
+      </CardContent>
+    </Card>
+
     </>
   )
 }
